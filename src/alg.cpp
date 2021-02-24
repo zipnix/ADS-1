dcc16b4e83473f8b630f5b17539841ed55a0dc18
anton <ashtanyuk@gmail.com>
2021-02-24 17:29:36 +0300

    Отредактирован tests.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,5 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value)
-{
-   //  поместить сюда реализацию алгоритма
+int cbinsearch(int *arr, int size, int value) {
+  //  поместить сюда реализацию алгоритма
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,33 +1,31 @@
-#include "gtest/gtest.h"
-int cbinsearch(int *arr, int size, int value);
+// Copyright 2021 NNTU-CS
+
+#include <gtest/gtest.h>
+#include "alg.h"
 
 
-TEST(lab1, test1)
-{
+TEST(lab1, test1) {
     int arr[] = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5};
     int count = cbinsearch(arr, 15, 3);
     EXPECT_EQ(3, count);
 }
 
-TEST(lab1, test2)
-{
+TEST(lab1, test2) {
     int arr[] = {1, 2, 2, 13, 13, 13, 14, 14, 14, 14, 15, 15, 15, 15, 15};
     int count=cbinsearch(arr, 15, 3);
     EXPECT_EQ(0, count);
 }
 
-TEST(lab1, test3)
-{
+TEST(lab1, test3) {
     int arr[] = {12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12};
     int count = cbinsearch(arr, 15, 12);
     EXPECT_EQ(15, count);
 }
 
-TEST(lab1, test4)
-{
+TEST(lab1, test4) {
     int size = 65536;
     int *arr = new int[size];
-    for(int i=0; i<size; i++)
+    for(int i = 0; i < size; i++)
         arr[i] = i;
     int count = cbinsearch(arr, size, 1024);
     delete arr;
