afee98b5ca91cf07ec4ef57b9660edea4348bc5b
zipnix <78799489+zipnix@users.noreply.github.com>
2021-03-21 14:44:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int score=0;
+  for(int i=0, i<=size, i++){
+  if(arr[i]=value){
+    score++;
+  }
+  if(score!=)
+    return score;
+  else
+  return 0;
 }
