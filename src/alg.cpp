c5ebe5ca67768b1e5978b8afd4e425a748b582a1
zipnix <78799489+zipnix@users.noreply.github.com>
2021-03-21 14:52:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,11 +2,11 @@
 
 int cbinsearch(int *arr, int size, int value) {
     int score=0;
-  for (int i=0, i<=size, i++) {
-  if (arr[i]=value) {
+  for (int i = 0, i <= size, i++) {
+  if (arr[i] = value) {
     score++;
   }
-  if (score!=)
+  if (score !=)
     return score;
   else
   return 0;
