5cb3aa207f1a96cb44cd0545af0e3bd152c10da4
zipnix <78799489+zipnix@users.noreply.github.com>
2021-03-22 17:48:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,16 +5,14 @@ int cbinsearch(int *arr, int size, int value) {
     while (left < right) {
         center = (left + right) / 2;
         if (arr[center] < value)
-            left = center++;
+            left = center + 1;
         else
             right = center;
     }
-    if (arr[left] == value) {
         while (arr[left] == value) {
             score++;
             left++;
         }
-    }
   if (score != 0)
       return score;
   else
